af0a6a115137f61737bae0fc9663214ed71f2b7a
Dylan Kauling <gunsmithy@gmail.com>
2016-08-22 04:33:10 -0400

    Prepared Jail Dockerfile and script
    Changed install script to attempt to setup the Gateone jail container
    from a the image based on the original Gateone Dockerfile.

--- install.sh
+++ install.sh
@@ -4,16 +4,15 @@ sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/cer
 sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/privkey.pem Wallpaper_Site:/etc/letsencrypt/live/wallpaperexploit.ddns.net/privkey.pem
 sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/chain.pem Wallpaper_Site:/etc/letsencrypt/live/wallpaperexploit.ddns.net/chain.pem
 sudo docker exec Wallpaper_Site service apache2 start
+cd ~
 git clone https://github.com/liftoff/GateOne.git # Clone the repo
 cd GateOne/docker
-sudo docker build -t gateone . # Always tag your builds!
-sudo docker run -t --name=Gateone -p 8000:8000 gateone
-sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/cert.pem Gateone:/etc/gateone/ssl/certificate.pem
-sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/privkey.pem Gateone:/etc/gateone/ssl/keyfile.pem
-# cd ..
-# cd ..
-# cd gateoneJail
-# sudo docker build -t gateone_jail .
-# sudo docker run -t --name=Gateone_Jail -p 8000:8000 gateone_jail
-# sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/cert.pem Gateone_Jail:/etc/gateone/ssl/certificate.pem
-# sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/privkey.pem Gateone_Jail:/etc/gateone/ssl/keyfile.pem
+sudo docker build -t gateone .
+# sudo docker run -d --name=Gateone -p 8000:8000 gateone
+# sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/cert.pem Gateone:/etc/gateone/ssl/certificate.pem
+# sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/privkey.pem Gateone:/etc/gateone/ssl/keyfile.pem
+cd ~/WallpaperExploitSite/gateoneJail
+sudo docker build -t gateone_jail .
+sudo docker run -d --name=Gateone_Jail -p 8000:8000 gateone_jail
+sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/cert.pem Gateone_Jail:/etc/gateone/ssl/certificate.pem
+sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/privkey.pem Gateone_Jail:/etc/gateone/ssl/keyfile.pem
