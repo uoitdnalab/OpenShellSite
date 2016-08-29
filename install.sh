a14a36c89c5704e59b3c20e1963d08969f25afa6
Dylan Kauling <gunsmithy@gmail.com>
2016-08-29 17:14:15 -0400

    Updated script and SSL for the new domain/host
    Going to be hosting on a separate VM now with wallpaper.nextproject.ca
    pointing to its IP, so have to get new certificates for the domain. Also
    not going to need the Nginx proxy anymore from my personal machine.

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
-# Install script to create the two containers and run them
-# Assumes Docker is already installed along with an Nginx proxy container and LetsEncrypt
+# Install script to create the four containers and run them
+# Assumes Docker is already installed along with LetsEncrypt
 # git clone https://github.com/Gunsmithy/WallpaperExploitSite.git && cd WallpaperExploitSite && chmod +x install.sh && ./install.sh
 sudo docker build -t "wallpaper_site" .
-sudo docker run -itd -e VIRTUAL_HOST=wallpaperexploit.ddns.net -p 443:443 --name Wallpaper_Site wallpaper_site:latest
-sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/cert.pem Wallpaper_Site:/etc/letsencrypt/live/wallpaperexploit.ddns.net/cert.pem
-sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/privkey.pem Wallpaper_Site:/etc/letsencrypt/live/wallpaperexploit.ddns.net/privkey.pem
-sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/chain.pem Wallpaper_Site:/etc/letsencrypt/live/wallpaperexploit.ddns.net/chain.pem
+sudo docker run -itd -p 80:80 -p 443:443 --name Wallpaper_Site wallpaper_site:latest
+sudo docker cp --follow-link /etc/letsencrypt/live/wallpaper.nextproject.ca/cert.pem Wallpaper_Site:/etc/letsencrypt/live/wallpaper.nextproject.ca/cert.pem
+sudo docker cp --follow-link /etc/letsencrypt/live/wallpaper.nextproject.ca/privkey.pem Wallpaper_Site:/etc/letsencrypt/live/wallpaper.nextproject.ca/privkey.pem
+sudo docker cp --follow-link /etc/letsencrypt/live/wallpaper.nextproject.ca/chain.pem Wallpaper_Site:/etc/letsencrypt/live/wallpaper.nextproject.ca/chain.pem
 sudo docker exec Wallpaper_Site service apache2 start
 cd ~
 git clone https://github.com/liftoff/GateOne.git # Clone the repo
 cd GateOne/docker
 sudo docker build -t gateone .
 sudo docker run -d --name=Gateone -p 8000:8000 gateone
-sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/cert.pem Gateone:/etc/gateone/ssl/certificate.pem
-sudo docker cp --follow-link /etc/letsencrypt/live/wallpaperexploit.ddns.net/privkey.pem Gateone:/etc/gateone/ssl/keyfile.pem
+sudo docker cp --follow-link /etc/letsencrypt/live/wallpaper.nextproject.ca/cert.pem Gateone:/etc/gateone/ssl/certificate.pem
+sudo docker cp --follow-link /etc/letsencrypt/live/wallpaper.nextproject.ca/privkey.pem Gateone:/etc/gateone/ssl/keyfile.pem
 
 cd ~/WallpaperExploitSite/gateoneJail
 sudo docker build -t gateone_jail .
