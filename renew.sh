5fc8c6d83ad44ad6f4a4430e61f36fff80294513
Dylan Kauling <gunsmithy@gmail.com>
2016-08-30 13:22:12 -0400

    Removed links from site, added renew script
    Removed links on the site to the source code and document upon Q's
    request. Added his email unless he opts to remove it. Added a script to
    renew the SSL certificates and restart the containers.

---
+++ renew.sh
@@ -0,0 +1,20 @@
+# Script to renew the certificates and restart the docker containers
+
+# Stop the containers using the certs
+docker stop Wallpaper_Site
+docker stop Gateone
+
+# Renew the certs
+letsencrypt renew --standalone --agree-tos --manual-public-ip-logging-ok
+
+# Copy the certs to the containers
+docker cp --follow-link /etc/letsencrypt/live/wallpaper.nextproject.ca/cert.pem Wallpaper_Site:/etc/letsencrypt/live/wallpaper.nextproject.ca/cert.pem
+docker cp --follow-link /etc/letsencrypt/live/wallpaper.nextproject.ca/privkey.pem Wallpaper_Site:/etc/letsencrypt/live/wallpaper.nextproject.ca/privkey.pem
+docker cp --follow-link /etc/letsencrypt/live/wallpaper.nextproject.ca/chain.pem Wallpaper_Site:/etc/letsencrypt/live/wallpaper.nextproject.ca/chain.pem
+docker cp --follow-link /etc/letsencrypt/live/wallpaper.nextproject.ca/cert.pem Gateone:/etc/gateone/ssl/certificate.pem
+docker cp --follow-link /etc/letsencrypt/live/wallpaper.nextproject.ca/privkey.pem Gateone:/etc/gateone/ssl/keyfile.pem
+
+# Start the containers again
+docker start Wallpaper_Site
+docker exec Wallpaper_Site service apache2 start
+docker start Gateone
