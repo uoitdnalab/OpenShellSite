7c1a719aee0e265c90eeab7260a431f5cfed1b4b
Dylan Kauling <gunsmithy@gmail.com>
2017-01-27 00:38:33 -0500

    Must run devpts mount inside container
    Removed devpts mount from build since you evidently can't run privileged
    commands in docker builds as of yet. Now must run it once the container
    is running. Changed the container run command to run as priviliged.

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ sudo docker cp --follow-link /etc/letsencrypt/live/wallpaper.nextproject.ca/priv
 
 cd ~/WallpaperExploitSite/gateoneJail
 sudo docker build -t gateone_jail .
-sudo docker run -itd --name=Gateone_Jail -p 2222:2222 -p 10000:10000 --restart=always gateone_jail
+sudo docker run -itd --name=Gateone_Jail -p 2222:2222 -p 10000:10000 --restart=always --privileged gateone_jail
 
 cd ~/WallpaperExploitSite/ftp_server
 sudo docker build -t ftp_server .
