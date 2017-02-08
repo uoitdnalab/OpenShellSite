5b56d0bbccaaabb88a5f35f5ad3927f951f215f8
Dylan Kauling <gunsmithy@gmail.com>
2017-02-08 09:40:07 -0500

    Missed some folder names
    Missed folder name changes in the install script.

--- install.sh
+++ install.sh
@@ -16,10 +16,10 @@ sudo docker run -d --name=Gateone -p 8000:8000 --restart=always gateone
 sudo docker cp --follow-link /etc/letsencrypt/live/openshell.nextproject.ca/cert.pem Gateone:/etc/gateone/ssl/certificate.pem
 sudo docker cp --follow-link /etc/letsencrypt/live/openshell.nextproject.ca/privkey.pem Gateone:/etc/gateone/ssl/keyfile.pem
 
-cd ~/WallpaperExploitSite/gateoneJail
+cd ~/OpenShellSite/gateoneJail
 sudo docker build -t gateone_jail .
 sudo docker run -itd --name=Gateone_Jail -p 23:23 -p 2222:2222 -p 10000:10000 -p 10001:10001 -p 10002:10002 -p 10003:10003 -p 10004:10004 -p 10005:10005 -p 10006:10006 -p 10007:10007 -p 10008:10008 -p 10009:10009 --restart=always --privileged gateone_jail
 
-cd ~/WallpaperExploitSite/ftp_server
+cd ~/OpenShellSite/ftp_server
 sudo docker build -t ftp_server .
 sudo docker run -itd -p 2121:2121 -p 21210-21220:21210-21220 --name FTP_Server --restart=always ftp_server
